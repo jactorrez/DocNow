953525a7c502c5e0566cb4c0b2be9a2380790cf8
Javier Torrez <javierctorrez@gmail.com>
2017-02-08 00:19:49 -0500

    finished hero section

--- src/js/components/pages/Home/index.js
+++ src/js/components/pages/Home/index.js
@@ -1,11 +1,22 @@
 import React from "react"; 
-import coverImg from "home-cover2";
+// import coverImg from "home-cover2";
 
 const Home = () => (
 	<div>
 		<main>
-			<header> 
-				<img src={coverImg}/>
+			<header className="hero-container"> 
+				<section className="hero">	
+					<ul className="slideshow">
+						<div className="slide" data-slide="1">
+							<h1>The Best Doctors Near You, For You</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+							   labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+							   nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+							   esse cillum dolore eu fugiat nulla pariatur.
+							</p>
+						</div>
+					</ul>		
+				</section>
 			</header>
 		</main>
 	</div>
